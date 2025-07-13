9d6b8fb6a1261906ee793d47ac7f650b890e576b
Fabian Castelo <fabiancasteloap18@gmail.com>
2025-07-12 23:33:00 -0300

    Inserção de escolha Impa ou Par

--- olaMundo.js
+++ olaMundo.js
@@ -1,1 +1,15 @@
 console.log('Olá Mundo!'); // meu primeiro olá mundo com o JS.
+
+/* 
+=============================================================================================
+                            Criação de identificação impa ou par.
+=============================================================================================
+*/
+
+let x = 4;
+
+if(x % 2 === 0){
+    console.log(x + " é um numero par.");
+}else{
+    console.log(x + " é um numero impar.");
+}
